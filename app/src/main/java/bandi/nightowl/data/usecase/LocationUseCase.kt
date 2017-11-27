062487a33ce2c5896e0b19c5e78cf6fa2a4cb387
Andreas Bauer <abauer@inovex.de>
2017-11-27 23:37:31 +0100

    Add location updates to LocationUseCase

--- app/src/main/java/bandi/nightowl/data/usecase/LocationUseCase.kt
+++ app/src/main/java/bandi/nightowl/data/usecase/LocationUseCase.kt
@@ -3,28 +3,43 @@ package bandi.nightowl.data.usecase
 import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
 
 
-class LocationUseCase(val context: Context) {
+
+
+class LocationUseCase(val context: Context) : LocationCallback() {
 
     private val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+    private val subject = PublishSubject.create<Location>()
+
+    fun listenToLocationRequests() : Observable<Location> {
+        return subject
+    }
 
     @SuppressLint("MissingPermission")
     fun getLocation() : Observable<Location> {
-        val subject = PublishSubject.create<Location>()
+        val locationListener = this
 
         // Wait for the GoogleApiClient to be connected
         fusedLocationClient.lastLocation.addOnCompleteListener {
-            subject.onNext(it.result)
-            subject.onComplete()
+            if(it.result == null) {
+                fusedLocationClient.requestLocationUpdates(LocationRequest.create(), locationListener, context.mainLooper)
+            } else {
+                subject.onNext(it.result)
+            }
         }
         return subject
 
     }
+
+    override fun onLocationResult(locationResult: LocationResult) {
+        subject.onNext(locationResult.lastLocation)
+        fusedLocationClient.removeLocationUpdates(this)
+    }
+
 
 
 }
