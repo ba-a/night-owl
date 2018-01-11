97dd8a4715ab68f5a034b7ae8130a8a6f4fcc636
Andreas Bauer <abauer@inovex.de>
2018-01-11 23:12:59 +0100

    Add logging interceptor for retrofit

--- app/src/main/java/bandi/nightowl/inject/AppModule.kt
+++ app/src/main/java/bandi/nightowl/inject/AppModule.kt
@@ -10,6 +10,10 @@ import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+
+
 
 /**
  * Created by abauer on 05.11.17.
@@ -30,8 +34,13 @@ class AppModule {
     @Singleton
     @Provides
     fun provideRetrofit(): Retrofit {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+
         return Retrofit.Builder()
                 .baseUrl("https://googleapi.com/")
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
